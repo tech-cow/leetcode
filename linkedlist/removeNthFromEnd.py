1e785cedff3e29600b34b1bb37bc55fb8bce7e09
Yu Zhou <junior147147@yahoo.com>
2017-04-16 22:51:35 -0700

    Update Remove Nth From End, read this about dummy node

--- linkedlist/removeNthFromEnd.py
+++ linkedlist/removeNthFromEnd.py
@@ -10,9 +10,18 @@ class Solution(object):
         for _ in xrange(n):
             fast = fast.next
 
-        # 我本来的代码没有15-16行，可以给我解释下这个edge case干了什么么
-        # 论坛里面都设置了dummynode，这题为什么要设置dummynode
-        # 先去玩狼人杀，回来继续研究
+        # 总算懂这个Cornor Case了
+        # 因为没有设置DummyNode，如果出现以下的这个Cornor Case
+        # 1 -> 2， 然后 N = 2
+        # 我们要做什么呢，删除1，然后返回2，也就是size of the list
+        # 但如何删除1，也就是Head，以下所有的代码都没有考虑到这个点
+        # 如果有个DummyHead，我们可以尽管的将DummyHead连接到Head，
+        # 然后将DummyHead指到2就行了，Head就删除了
+        # 若没有DummyHead，以下的代码无法处理Head被删除的Cornor Case
+
+        # 那么这个Cornor case就牛逼了
+        # 在没有Cornor Case的情况下，if not fast，也就意味着size就等于N
+        # 直接返回head.next，也就意味着删除了Head，牛不牛！？
         if not fast:
             return head.next
         while fast.next:
