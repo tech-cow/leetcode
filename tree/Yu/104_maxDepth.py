455238fd42b126c0db3841e0ee3625b91d7e68c4
Yu Zhou <junior147147@yahoo.com>
2017-04-26 13:31:11 -0700

    104

---
+++ tree/Yu/104_maxDepth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Author: Yu Zhou
+
+# ****************
+# Descrption:
+# Given a binary tree, find its maximum depth.
+# ****************
+
+# 思路：
+# 基础题，确保有Root就行。然后递归
+
+# ****************
+# Final Solution *
+# ****************
+class Solution(object):
+    def maxDepth(self, root):
+      if not root:
+          return 0
+      else:
+          return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
