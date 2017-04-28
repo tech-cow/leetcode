d9c4d552ba19dac44912ca58b93bf95e7dd36c73
Yu Zhou <junior147147@yahoo.com>
2017-04-28 16:01:21 -0700

    112

---
+++ tree/Yu/112_hasPathSum.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Author: Yu Zhou
+# ****************
+# Descrption:
+# 112. Path Sum
+# Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+# ****************
+
+# 思路
+# 每次向下递归要确保root.left和root.right都是None
+# 像上返回的时候对Sum和Root.Val进行比对
+
+# 本人的中文视频讲解：
+# https://www.youtube.com/watch?v=LgtcGjIuE18&feature=youtu.be
+
+
+
+# ****************
+# Final Solution *
+# ****************
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+
+        if root.left == None and root.right == None:
+            return root.val == sum
+
+        left = self.hasPathSum(root.left, sum - root.val)
+        right = self.hasPathSum(root.right, sum - root.val)
+
+        return left or right
