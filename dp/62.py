f66692cbc8e249559e4bd1303d1fc0899d188324
Yu Zhou <junior147147@yahoo.com>
2017-05-26 14:24:43 -0700

    comment on 62

--- dp/62.py
+++ dp/62.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Author: Yu Zhou
+# 62. Unique Path
+
+# 解题思路：
+# 先解决横向竖向第一列，全部设成1，用了个小trick把所有的DP矩阵都设成了1
+# 状态：计算从前到现在节点为止所有的路劲
+# 状态转移方程：f[x][y] = f[x-1][y] + f[x][y-1]
+# 最后返回右下角的数就行。
+
 class Solution(object):
     def uniquePaths(self, m, n):
         """
@@ -5,6 +17,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+
         #edge:
         if not m or not n:
             return 0
