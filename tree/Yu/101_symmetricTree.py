9f1d69d72c29e7546f08a00043d218d945c2bb2b
Yu Zhou <junior147147@yahoo.com>
2017-10-12 15:34:15 -0700

    Clean up code

--- tree/Yu/101_symmetricTree.py
+++ tree/Yu/101_symmetricTree.py
@@ -1,29 +1,35 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-# Author: Yu Zhou
-# ****************
-# Descrption:
-# 101. Symmetric Tree
-# Given a binary tree, check whether it is a mirror of itself
-# (ie, symmetric around its center).
-# ****************
-
-# 思路：
-# 这道题只要知道一个规律,就是左边Child要等于右边Child，就很容易了
-# 先解决Root的Edge，之后在对其他进行一个统一处理，我选择写一个Helper，也可以不写
-
-# if not left or not right: return left == right的意思是：
-        # if not left or not right: return False
-        # if not left and not right: return True
-
 class Solution(object):
+    # Queue
     def isSymmetric(self, root):
+        if not root:
+            return True
+        queue = [(root.left, root.right)]
+        while queue:
+            left, right = queue.pop(0)
 
-        if not root: return True
+            if not left and not right:
+                continue
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+            queue.append((left.left, right.right))
+            queue.append((left.right,right.left))
+        return True        
+        
+        
+    # Recursion
+    def isSymmetric2(self, root):
+        if not root:
+            return True
+
         def dfs(left, right):
-            if not left or not right: return left == right
-            if left.val != right.val: return False
-            return dfs(left.left, right.right) and dfs(left.right, right.left)
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False    
+            return dfs(left.left , right.right) and dfs(left.right, right.left)
 
         return dfs(root.left, root.right)
